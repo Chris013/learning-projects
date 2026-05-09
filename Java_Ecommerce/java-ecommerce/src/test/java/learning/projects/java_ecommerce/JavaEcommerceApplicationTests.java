f307c56e459786acf03fe52f8c30c2facd8b69b6
Chris013 <chris.schlegel1@t-online.de>
2026-05-09 13:49:50 +0200

    Initialized Spring

---
+++ Java_Ecommerce/java-ecommerce/src/test/java/learning/projects/java_ecommerce/JavaEcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package learning.projects.java_ecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaEcommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

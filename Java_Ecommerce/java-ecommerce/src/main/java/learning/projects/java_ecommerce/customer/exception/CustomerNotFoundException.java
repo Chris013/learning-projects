17c803c2b63f3e79e5356fb48f3e7bed2e07858f
Chris013 <chris.schlegel1@t-online.de>
2026-05-13 16:52:02 +0200

    feat: added customer not found exception

---
+++ Java_Ecommerce/java-ecommerce/src/main/java/learning/projects/java_ecommerce/customer/exception/CustomerNotFoundException.java
@@ -0,0 +1,19 @@
+package learning.projects.java_ecommerce.customer.exception;
+
+import java.util.UUID;
+
+import org.springframework.http.HttpStatus;
+
+import learning.projects.java_ecommerce.common.exception.DomainException;
+
+public class CustomerNotFoundException extends DomainException{
+
+    protected CustomerNotFoundException(UUID id) {
+        super(
+            "CUSTOMER_NOT_FOUND", 
+            "Customer not found: " + id, 
+            HttpStatus.NOT_FOUND
+        );
+    }
+
+}
